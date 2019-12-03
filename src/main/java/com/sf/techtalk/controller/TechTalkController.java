b0f44a02ccf76cda9dc35b04362b8f35d0856c05
Avinash Kumar <avinashkumar@avkumar-ltm1.internal.salesforce.com>
2019-12-03 11:17:04 +0530

    initial code

---
+++ src/main/java/com/sf/techtalk/controller/TechTalkController.java
@@ -0,0 +1,23 @@
+package com.sf.techtalk.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/say-hello")
+public class TechTalkController {
+
+	Logger logger = LoggerFactory.getLogger(TechTalkController.class);
+	
+	@GetMapping
+	public String sayHello() {
+		
+		return "Hello Tech Talk";
+	}
+	
+	
+}
---
+++ src/test/java/com/sf/techtalk/FoxyBrainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sf.techtalk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoxyBrainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
